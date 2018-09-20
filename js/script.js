42168c0c4877f5c22cc1f34fdfdc70768458c960
Leonel Oliveira - OnResearch Inc <loliveira@hq.onresearch.com>
2018-09-20 16:39:52 -0400

    updates

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let resultVal = '';
+let hasError = true;
+const errorMsg = ['Attention','Please select an option!'];
+const errorMsg2 = ['=(','You can not go back. Sorry!'];
+
+let radios = document.getElementsByName('radio');
+let card = document.getElementsByClassName("card");
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener("click", function(){    
+    if(radios[i].checked){
+      resultVal = radios[i].value; 
+      for (let i = 0; i < card.length; i++) {
+        card[i].classList.remove('selected');
+      }
+      card[i].classList.add("selected");
+    }  
+  });
+}
+const btn = document.getElementById("submit_button");
+btn.addEventListener("click", function() {
+  if(resultVal){
+    alert(resultVal);
+  }else{
+    	document.getElementsByClassName('modal-title')[0].innerText = errorMsg[0];
+		  document.getElementsByClassName('modal-body')[0].innerText = errorMsg[1];
+      $("#errorModal").modal();
+  }
+});
+const btnBack = document.getElementById("back_button");
+btnBack.addEventListener("click", function() {
+  document.getElementsByClassName('modal-title')[0].innerText = errorMsg2[0];
+  document.getElementsByClassName('modal-body')[0].innerText = errorMsg2[1];
+  $("#errorModal").modal();
+});
